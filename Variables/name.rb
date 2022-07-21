0da42e99cceb0cc306b4eba732da1916c0f1a205
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2022-07-21 00:06:04 -0400

    Exercises for Variables Chapter

---
+++ Variables/name.rb
@@ -0,0 +1,13 @@
+# name.rb
+
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+# puts "Hi #{name}! Welcome to the Jungle"
+# # 10.times {puts name}
+# 10.times do 
+#   puts name
+# end 
+
+puts first_name + " " + last_name
